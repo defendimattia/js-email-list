aed384049b8d906236ef0a82a2d9c03147d13146
defendimattia <defendi.mattia92@gmail.com>
2025-03-13 15:34:04 +0100

    add: bootstrap and style

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const source = "https://flynn.boolean.careers/exercises/api/random/mail"
 for (i = 0; i < 10; i++) {
     axios.get(source)
         .then(response => {
-            ulElement.innerHTML += `<li>${response.data.response}</li>`
+            ulElement.innerHTML += ` <li class="list-group-item">${response.data.response}</li>`
         })
         .catch(error => {
             console.error(error)
