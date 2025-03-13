c2c8b22481cf45c91d478e3099c0c8efdee51a63
defendimattia <defendi.mattia92@gmail.com>
2025-03-13 16:20:48 +0100

    add: function

--- js/main.js
+++ js/main.js
@@ -6,21 +6,22 @@ const buttonElement = document.querySelector("button")
 const source = "https://flynn.boolean.careers/exercises/api/random/mail"
 
 
-for (i = 0; i < 10; i++) {
-    axios.get(source)
-        .then(response => {
-            ulElement.innerHTML += ` <li class="list-group-item">${response.data.response}</li>`
-        })
-        .catch(error => {
-            console.error(error)
-        })
-}
+genAndAddTenEmails()
 
 
 buttonElement.addEventListener("click", (event) => {
 
     ulElement.innerHTML = ""
     
+    genAndAddTenEmails()
+
+    
+})
+
+
+
+function genAndAddTenEmails() {
+
     for (i = 0; i < 10; i++) {
         axios.get(source)
             .then(response => {
@@ -30,4 +31,4 @@ buttonElement.addEventListener("click", (event) => {
                 console.error(error)
             })
     }
-});
+}
