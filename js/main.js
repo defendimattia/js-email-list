a35ca1ce704f8302483c59e7f64f7459be4cc808
defendimattia <defendi.mattia92@gmail.com>
2025-03-13 15:39:25 +0100

    add: bonus

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 const ulElement = document.querySelector("ul")
+const buttonElement = document.querySelector("button")
 
 
 const source = "https://flynn.boolean.careers/exercises/api/random/mail"
@@ -16,3 +17,17 @@ for (i = 0; i < 10; i++) {
 }
 
 
+buttonElement.addEventListener("click", (event) => {
+
+    ulElement.innerHTML = ""
+    
+    for (i = 0; i < 10; i++) {
+        axios.get(source)
+            .then(response => {
+                ulElement.innerHTML += ` <li class="list-group-item">${response.data.response}</li>`
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+});
